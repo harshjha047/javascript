07ee9c65028eba8ed527a54112fd9c6e230ab001
harshjha047 <spyderhj11@gmail.com>
2024-07-23 22:35:28 +0530

    datatype

---
+++ day2.js
@@ -0,0 +1,16 @@
+const acountId = 47 ;
+let userName = 'Harsh Jha';
+var city = 'noida'
+pin ='201301'
+let state;
+
+console.log(acountId, userName, city, pin, state)
+
+// dont use var 
+
+// const acountId = 47 ;
+userName = "Harsh Jha";
+city = 'dalhi'
+// pin ;
+state =  'delhi'
+console.table([acountId, userName, city, pin, state]) // new
